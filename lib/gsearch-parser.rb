16f8bfacd6b5caf37649c093e415181895f9e0c6
Zhuhao Wang <dofelw@gmail.com>
2013-11-27 15:04:50 +0800

    Revert "decode title"
    This reverts commit e7bc87eea53b4cfb5d24b7ec8bdd84f11950e151.

--- lib/gsearch-parser.rb
+++ lib/gsearch-parser.rb
@@ -80,7 +80,6 @@ class GoogleWebSearch
       begin
       # Extract the title
       title = result.css('h3 a').first.inner_html
-      title = URI.decode title
 
       # Extract the content. There is the possibility for
       # the content to be nil, so check for this
